5bd988d2f38ed5cbe68cb14d59acc5e96b1f562b
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2021-11-19 12:44:32 +0330

    End Ptoject

---
+++ playground.js
@@ -0,0 +1,35 @@
+// const fs = require("fs");
+
+// const persons = [
+//   { id: 1, fullname: "Erfan Pouladzadeh" },
+//   { id: 2, fullname: "Yasaman Pouladzadeh" },
+//   { id: 3, fullname: "Arefh Forgani" },
+// ];
+
+// fs.writeFileSync("contacts.json", JSON.stringify(persons));
+
+// const data = fs.readFileSync("contacts.json");
+// const p = JSON.parse(data.toString());
+// console.log(p);
+// console.log(typeof p);
+
+// ====================================================
+
+// const commands = process.argv[2];
+
+// if (commands == "add") {
+//   console.log("Adding ...");
+// } else {
+//   console.log("Command not valid.");
+// }
+
+// switch (commands) {
+//   case "add":
+//     console.log("Adding ...");
+//     break;
+//   case "remove":
+//     console.log("Removing ...");
+//     break;
+//   default:
+//     console.log("Command not valid.");
+// }
